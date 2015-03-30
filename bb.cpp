5822f36d56b8e9f376d687d62fd156a24cd0b0ce
Dallas Nicholson <nicholson.dallasc@gmail.com>
2015-03-29 19:39:15 -0500

    created bb.cpp, defined things we will need later on

---
+++ bb.cpp
@@ -0,0 +1,19 @@
+#include <ncurses>
+
+
+/*
+  Playing field
+  2 player
+     |_ paddle
+     |_ set of tiles
+     |_ score
+     |_ socket
+  Ball
+ */
+
+int main(int argc, char * argv[]) {
+  
+
+
+  return 0;
+}
