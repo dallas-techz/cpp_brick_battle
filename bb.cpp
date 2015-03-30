1d3b29eb5eedab5c9977fcd3680501f5c0bf2a23
Dallas Nicholson <nicholson.dallasc@gmail.com>
2015-03-30 01:15:56 -0500

    set up changed a bit with entities.cpp in the paddle class, also began to define some values in bb.cpp

--- bb.cpp
+++ bb.cpp
@@ -1,5 +1,15 @@
 #include <ncurses>
 
+#ifndef BRICKBATTLE_MAIN
+#define BRICKBATTLE_MAIN
+
+#define TOTAL_SCREEN_WIDTH 160
+#define TOTAL_SCREEN_HEIGHT 30
+#define GAME_SCREEN_WIDTH 160
+#define GAME_SCREEN_HEIGHT 28
+#define HEADER_SCREEN_WIDTH 160
+#define HEADER_SCREEN_HEIGHT 2
+
 
 /*
   Playing field
@@ -17,3 +27,5 @@ int main(int argc, char * argv[]) {
 
   return 0;
 }
+
+#endif
